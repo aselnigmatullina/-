a9cbb7dd9f80619e2dc5044e782af47c141211a2
aselnigmatullina <nigmatullina.asel2016@yandex.ru>
2019-09-19 14:08:28 +0300

    code changed

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
-//Домашнее задание: расчет рулонов
-//Вычисляем периметр комнаты
-let roomWidth = 5;
-let roomLenght = 6;
-let perimetr = (roomLenght+roomWidth)*2;
+//Home work: calculation of rolls
+//Calculate the perimeter of the room
+const roomWidth = 5;
+const roomLength = 6;
+const perimetr = (roomLength+roomWidth)*2;
 console.log(perimetr + "м");
 
-//сколько полотнищ понадобится для оклеивания комнаты?
-let wallpaperWidth = 1.06;
-let wallpaperAmount=Math.round(perimetr/wallpaperWidth);
-
+//The amount of wallpapers required for pasting the room
+const wallpaperWidth = 1.06;
+const wallpaperAmount=Math.round(perimetr/wallpaperWidth);
 console.log(wallpaperAmount);
 
-//На сколько полотнищ хватит одного рулона?
-let rollLength = 10;
-let roomHeight = 2.75;
-let roomHeightMore = 0.1;
-let roomHeightFinish=(roomHeight+roomHeightMore);
-let wallpaperInRoll=Math.floor(rollLength/roomHeightFinish);
+//How many sheets will one roll have?
+const rollLength = 10;
+const roomHeight = 2.75;
+const roomHeightMore = 0.1;
+const roomHeightFinish=(roomHeight+roomHeightMore);
+const wallpaperInRoll=Math.floor(rollLength/roomHeightFinish);
 console.log(wallpaperInRoll);
 
-//Сколько рулонов обоев понадобится?
-let rollAmount=Math.floor(wallpaperAmount/wallpaperInRoll);
+//How many rolls of wallpaper will you need?
+const rollAmount=Math.floor(wallpaperAmount/wallpaperInRoll);
 console.log(rollAmount);
 
 
